44da704610aa340ed789dc0c78e9a052bd4231a9
9172175295 <150075363+9172175295@users.noreply.github.com>
2023-11-07 09:30:44 +0530

    Add files via upload

---
+++ List.jsx
@@ -0,0 +1,57 @@
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { userActions } from '_store';
+
+export { List };
+
+function List() {
+    const users = useSelector(x => x.users.list);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(userActions.getAll());
+    }, []);
+
+    return (
+        <div>
+            <h1>Users</h1>
+            <Link to="add" className="btn btn-sm btn-success mb-2">Add User</Link>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        {/* <th style={{ width: '50%' }}>Email</th> */}
+                        <th style={{ width: '80%' }}>Username</th>
+                        <th style={{ width: '20%' }}></th>
+                    </tr>
+                </thead>
+                <tbody>
+               
+                    {users?.value?.map(user =>
+                        <tr key={user.id}>
+                            {/* <td>{user.email}</td> */}
+                            <td>{user.username}</td>
+                            <td style={{ whiteSpace: 'nowrap' }}>
+                                <Link to={`edit/${user.id}`} className="btn btn-sm btn-primary me-1">Edit</Link>
+                                <button onClick={() => dispatch(userActions.delete(user.id))} className="btn btn-sm btn-danger" style={{ width: '60px' }} disabled={user.isDeleting}>
+                                    {user.isDeleting 
+                                        ? <span className="spinner-border spinner-border-sm"></span>
+                                        : <span>Delete</span>
+                                    }
+                                </button>
+                            </td>
+                        </tr>
+                    )}
+                    {users?.loading &&
+                        <tr>
+                            <td colSpan="4" className="text-center">
+                                <span className="spinner-border spinner-border-lg align-center"></span>
+                            </td>
+                        </tr>
+                    }
+                </tbody>
+            </table>
+        </div>
+    );
+}
